0dd62084f69813d11cbacae7751039dca7115b29
tanyudii <tanyudii@users.noreply.github.com>
2021-09-17 19:20:06 +0000

    deploy: 924c596b537d2c832614e4f975b92b2183d07f50

--- _nuxt/static/1631384008/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631384008",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"31bd607f"}}}}(null,"\u002F"));

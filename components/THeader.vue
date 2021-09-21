19a8830e19c24138649cd47db3e378f1ab43e7ac
Yudi Hertanto <39409106+tanyudii@users.noreply.github.com>
2021-09-21 18:56:24 +0700

    Update THeader.vue

--- components/THeader.vue
+++ components/THeader.vue
@@ -5,7 +5,7 @@
         <nuxt-link to="/">
           <div class="flex items-center space-x-2">
             <div class="border-r-2 border-gray-300">
-              <img src="/logo.png" alt="icon" class="w-8" />
+              <img src="/logo.png" alt="icon" class="w-8" loading="lazy" />
             </div>
             <div>
               <p class="text-xl text-gray-500 font-bold">Yudi Hertanto</p>
